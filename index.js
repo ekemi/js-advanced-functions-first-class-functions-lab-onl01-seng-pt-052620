56f41155d778d9fd55eb4e431bf1cb15f57ee2ba
edmundo papi <edmundopierre@yahoo.com>
2020-12-25 22:10:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const createFareMultiplier=function (arg){
     return arg *va;
   }
 }
-const fareDoubler = function(n){
-  return createFareMultiplier()(n)
+const fareDoubler = function(f){
+  return createFareMultiplier(f)(2)
 }
