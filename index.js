36c1638b9261e4a0432ea9193fea1779f117b886
edmundo papi <edmundopierre@yahoo.com>
2020-12-25 21:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(arr){
-  let emp = []
-  for (let i =0; i <=2;i++){
-    emp.push(arr[i])
-    }
-    return emp
-  }
+  return arr.slice(0,3);
+}
+  
