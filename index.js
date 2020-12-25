f3b26aa7b4ea7aa5d2a4b5580ab883e51c4446d1
edmundo papi <edmundopierre@yahoo.com>
2020-12-25 20:51:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(arr){
   let emp = []
-  for (let i =0, i <=2;i++){
+  for (let i =0; i <=2;i++){
     emp.push(arr[i])
     }
     return emp
